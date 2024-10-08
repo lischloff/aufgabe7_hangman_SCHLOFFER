14a591a47e1d48889c0f8ac3752b6854319433ef
lischloff <lisa.schloffer@gmail.com>
2024-10-08 08:33:23 +0200

    First Commit: Erstellen des GUIs

--- Aufgabe7_hangman/src/hangmanView/hangmanView.java
+++ Aufgabe7_hangman/src/hangmanView/hangmanView.java
@@ -1,7 +1,23 @@
+//Project: Hangman
+//Author: Schloffer Lisa
+//Date: 08.10.2024
+
 package hangmanView;
 
 import javax.swing.*;
 
 public class hangmanView {
     private JTextField wordField;
+    private JTextArea guessedLetters;
+    private JPanel hangmanPanel;
+    private JLabel hangmanPicture;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("hangmanView");
+        hangmanView hangmanViewInstance = new hangmanView();
+        frame.setContentPane(hangmanViewInstance.hangmanPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
