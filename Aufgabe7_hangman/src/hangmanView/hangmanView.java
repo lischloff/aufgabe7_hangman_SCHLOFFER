89b7608bf07d5344d27aa68a9a690aed6e1b879b
lischloff <lisa.schloffer@gmail.com>
2024-10-08 08:51:20 +0200

    Second Commit: Update vom GUI (habe ein weiteres Textfield hinzugefügt) und ich habe ein Wort als HiddenWord erstellt

--- Aufgabe7_hangman/src/hangmanView/hangmanView.java
+++ Aufgabe7_hangman/src/hangmanView/hangmanView.java
@@ -11,6 +11,27 @@ public class hangmanView {
     private JTextArea guessedLetters;
     private JPanel hangmanPanel;
     private JLabel hangmanPicture;
+    private JTextField buchstabenEingabe;
+
+    private String wordToGuess = "Fenster"; //Das Wort zu erraten
+
+    public hangmanView() {
+    wordField.setText(getHiddenWord(wordToGuess)); // Das Textfeld wird beim Erstellen des Objekts direkt mit den Unterstrichen befüllt
+}
+// Methode zur Umwandlung des Wortes in eine Zeichenkette mit Unterstrichen
+private String getHiddenWord(String word) {
+    // Erstellt einen StringBuilder für die Unterstrichdarstellung
+    StringBuilder hiddenWord = new StringBuilder();
+
+    // Durchlauft jedes Zeichen im Wort und füge einen Unterstrich hinzu
+    for (int i = 0; i < word.length(); i++) {
+        hiddenWord.append("_");
+        if (i < word.length() - 1) {
+            hiddenWord.append(" "); // Fügt ein Leerzeichen zwischen die Unterstriche hinzu
+        }
+    }
+    return hiddenWord.toString();  // Gibt die Zeichenkette mit den Unterstrichen zurück
+}
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("hangmanView");
